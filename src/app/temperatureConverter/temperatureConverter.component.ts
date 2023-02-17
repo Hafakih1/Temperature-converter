7505ab704ab3d28c22a9ecffa91bf4e411b920b8
Admin <admin.pull@hackerrank.workspace>
2023-02-17 03:54:18 +0000

    Auto save changes before Submit
    Commit by HackerRank

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -1,16 +1,29 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'temperature-converter',
-  templateUrl: './temperatureConverter.component.html',
-  styleUrls: ['./temperatureConverter.component.scss']
+  selector: "temperature-converter",
+  templateUrl: "./temperatureConverter.component.html",
+  styleUrls: ["./temperatureConverter.component.scss"],
 })
-
 export class TemperatureConverter implements OnInit {
+  c = "";
+  f = "";
+  constructor() {}
 
-  ngOnInit() {
-    // C = (F − 32) × 5/9
-    // F = C*9/5 + 32
+  ngOnInit() {}
+
+  onChange(value: string | null, type: "c" | "f") {
+    if (value === null) {
+      this.c = "";
+      this.f = "";
+      return;
+    }
+
+    const temperature = Number(value);
+    if (type === "c") {
+      this.f = ((temperature * 9) / 5 + 32).toFixed(1);
+    } else {
+      this.c = (((temperature - 32) * 5) / 9).toFixed(1);
+    }
   }
-
 }
